b7c09f309e28afc7e40b81ff9e02dc20812e49a5
nbrustein <nbrustein@gmail.com>
2013-06-25 17:23:32 -0400

    got rails and hbase all set up

---
+++ learning_platform/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearningPlatform::Application.config.secret_key_base = '3818f0f5526c82df3aee141c00e106d2cba9c1575225b8e0ba3b0b5c0f6db30451dd2c0d4468d95bb95b980190be3e9040af88c504bfb7ae983afd822a98622a'
